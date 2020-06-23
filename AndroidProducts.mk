13cc3e602da142db0a74933ada8db0667af2d675
KenHV <kenhv@null.net>
2020-06-23 16:41:05 +0530

    vince: bringup hentaiOS
    Signed-off-by: KenHV <kenhv@null.net>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2018 The havocOS Project
+# Copyright (C) 2017-2018 The hentaiOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_vince.mk
+    $(LOCAL_DIR)/hentai_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_vince-user \
-    havoc_vince-userdebug \
-    havoc_vince-eng
+    hentai_vince-user \
+    hentai_vince-userdebug \
+    hentai_vince-eng
