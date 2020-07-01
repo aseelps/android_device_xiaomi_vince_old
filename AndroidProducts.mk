44832fcecb5766141f7bd2905214085945657a79
KenHV <kenhv@null.net>
2020-07-01 13:29:43 +0530

    vince: fluid bringup
    finally
    
    Signed-off-by: KenHV <kenhv@null.net>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2018 The havocOS Project
+# Copyright (C) 2020 The fluidOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_vince.mk
+    $(LOCAL_DIR)/fluid_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_vince-user \
-    havoc_vince-userdebug \
-    havoc_vince-eng
+    fluid_vince-user \
+    fluid_vince-userdebug \
+    fluid_vince-eng
